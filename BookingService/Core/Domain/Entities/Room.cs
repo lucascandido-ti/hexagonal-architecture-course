4a31370515c66bcab1ac123c9b2ef5cc5d0c0d3d
lucascandido-ti <lucascandido.ti@gmail.com>
2023-04-30 17:49:06 -0300

    Configuração inicial do projeto de MicroServiço com DDH

---
+++ BookingService/Core/Domain/Entities/Room.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Room
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public bool InMaintenance { get; set; }
+
+        public bool isAvalible
+        {
+            get
+            {
+                if(this.InMaintenance || this.HasGuest)
+                {
+                    return false;
+                }
+                return true;
+            }
+        }
+
+        public bool HasGuest
+        {
+            get
+            {
+                return true
+            }
+        }
+    }
+}
